52866e1c7a07bbcccd0b40efa114efe3e587583f
Eduri Raja <raja.eduri@gmail.com>
2024-01-09 12:11:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,12 @@ from IPython.display import clear_output
 # !export LIBRARY_PATH="/usr/local/cuda/lib64/stubs"
 # !ldconfig /usr/lib64-nvidia
 
-!git clone https://github.com/dvmazur/mixtral-offloading.git --quiet
+# !git clone https://github.com/dvmazur/mixtral-offloading.git --quiet
+
+import subprocess
+# Clone the GitHub repository
+subprocess.run(['git', 'clone', 'https://github.com/dvmazur/mixtral-offloading.git', '--quiet'])
+
 !cd mixtral-offloading && pip install -q -r requirements.txt
 !huggingface-cli download lavawolfiee/Mixtral-8x7B-Instruct-v0.1-offloading-demo --quiet --local-dir Mixtral-8x7B-Instruct-v0.1-offloading-demo
 
